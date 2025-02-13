c717ff16f5843e36232d90763c8216ed04f7fc3e
Paul Lerner <lerner@tuta.io>
2025-02-13 17:01:03 +0100

    chore: setup + requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
+import pkg_resources
+import os
 
 setup(
     name='Bertalign',
@@ -6,5 +8,10 @@ setup(
     url='https://github.com/bfsujason/bertalign',
     description='An automatic mulitlingual sentence aligner.',
     packages=find_packages(),    
-    install_requires=[],
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ]
 )
